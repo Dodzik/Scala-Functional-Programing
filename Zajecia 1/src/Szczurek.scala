9eef00fef4a3920f50b9dd0913dbb7e3589bbf4f
Dodzik <przemoaaa@gmail.com>
2020-10-07 15:37:37 +0200

    test

---
+++ Zajecia 1/src/Szczurek.scala
@@ -0,0 +1,6 @@
+object Szczurek {
+  def main(args: Array[String]): Unit = {
+    println("Hello im RAAAAAAAAAT!!" + "\n"+"Do you have cheeessseee????????")
+  }
+
+}
---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Scala-FpiS-2020"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
